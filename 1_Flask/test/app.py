83cdb2f451b3e737fbe09f86eb6783a72c9e313b
hhhi21g <xuyuetong2025@163.com>
2025-07-07 13:23:15 +0800

    flask

--- 1_Flask/test/app.py
+++ 1_Flask/test/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)  # 创建Flask应用
 
@@ -8,5 +8,13 @@ def index():
     return render_template('index.html', title='一周走势页面')
 
 
+@app.route('/api/data')
+def data():
+    days = [1, 2, 3, 4, 5, 6, 7]
+    sales = [120, 200, 150, 80, 70, 110, 130]
+    return jsonify({'days': days, 'sales': sales})
+
+
 if __name__ == '__main__':
-    app.run(debug=True)  # 开启开发模式,默认在http://127.0.0.1:5000
+    # 允许其他主机使用本机IP进行访问,同一局域网
+    app.run(debug=True, host='0.0.0.0', port=5000)  # 开启开发模式,默认在http://127.0.0.1:5000
